37d6f98581c2b705be95cc1d32e0342878afcb9b
1tylerhhighland@gmail.com <1tylerhhighland@gmail.com>
2026-08-19 14:55:16 -0500

    Final mini-task update

--- Tyler_Lab1/Tyler_Lab1.cpp
+++ Tyler_Lab1/Tyler_Lab1.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Hello from Tyler! This is my first Git commit." << endl;
     cout << "Git makes collaboration easy!" << endl;
     cout << "Changes made by Chase Chisholm" << endl;
+    cout << "Lab 1 completed on 8/19/2026!" << endl;
 }
 //Edited directly on GitHub
