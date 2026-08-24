b1da6fc099aed6f285cd936d44281ae78de7352f
Archivist <1tylerhhighland@gmail.com>
2026-08-24 14:49:37 -0500

    Revert "Add student info display"

--- Tyler_Lab1/Tyler_Lab1.cpp
+++ Tyler_Lab1/Tyler_Lab1.cpp
@@ -10,6 +10,5 @@ int main()
     cout << "Changes made by Chase Chisholm" << endl;
     cout << "Lab 1 completed on 8/19/2026!" << endl;
     cout << "Feature by Tyler: Branching is awesome!" << endl;
-    cout << "Student ID: 1234567689 | Name: Tyler" << endl;
 }
 //Edited directly on GitHub
