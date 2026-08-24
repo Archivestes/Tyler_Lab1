39105bf28acb9cfe2cbf1de17b087493a552a399
1tylerhhighland@gmail.com <1tylerhhighland@gmail.com>
2026-08-24 14:08:00 -0500

    Add greeting feature on feature branch

--- Tyler_Lab1/Tyler_Lab1.cpp
+++ Tyler_Lab1/Tyler_Lab1.cpp
@@ -8,5 +8,6 @@ int main()
     cout << "Git makes collaboration easy!" << endl;
     cout << "Changes made by Chase Chisholm" << endl;
     cout << "Lab 1 completed on 8/19/2026!" << endl;
+    cout << "Feature by Tyler: Branching is awesome!" << endl;
 }
 //Edited directly on GitHub
