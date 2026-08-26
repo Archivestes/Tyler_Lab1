6932578365526c3583123b746363e43ff568189d
CJ Chisholm <iamcjc2005@gmail.com>
2026-08-26 14:20:07 -0500

    Grade-Calculator feature complete

--- Tyler_Lab1/Tyler_Lab1.cpp
+++ Tyler_Lab1/Tyler_Lab1.cpp
@@ -2,13 +2,27 @@
 #include <iostream>
 using namespace std;
 
+// Feature by Chase: Calculates the GPA of a student based on their grades in Math, English, and Science
 int main()
 {
-    cout << "Hello from Tyler." << endl;
-    cout << "Hello from Chase." << endl;
-    cout << "Git makes collaboration easy!" << endl;
-    cout << "Changes made by Chase Chisholm" << endl;
-    cout << "Lab 1 completed on 8/19/2026!" << endl;
-    cout << "Feature by Tyler: Branching is awesome!" << endl;
+	int math;
+	int english;
+	int science;
+	double gpa{};
+
+
+	cout << "Enter your Math Grade:" << endl;
+	cin >> math;
+
+	cout << "Enter your English Grade:" << endl;
+	cin >> english;
+
+	cout << "Enter your Science Grade:" << endl;
+	cin >> science;
+
+	gpa = (math + english + science) / 3;
+	cout << "Your GPA is: " << gpa << endl;
+
+
 }
 //Edited directly on GitHub
